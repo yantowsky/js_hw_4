6eb2301b6ed8e505aa5639cd68111dc592248a50
yantowsky <it.yantowsky@gmail.com>
2024-05-13 21:47:22 +0300

    Created HomeWork#4 (4.4)

--- main.js
+++ main.js
@@ -24,3 +24,21 @@ if (Number.isInteger(res)) {
 } else {
     console.log(`${num} - в цьому тризначному числі немає однакових цифр`);
 }
+
+//HomeWork_4.4
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr)):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
