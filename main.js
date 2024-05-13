8de5030c781ec6e4544c29232242c56452a4c525
yantowsky <it.yantowsky@gmail.com>
2024-05-13 21:09:34 +0300

    Changed the variable in the condition

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ do {
 res = num / 111;
 arrNum = num.toString().split('');
 
-if (Number.isInteger(num / 111)) {
+if (Number.isInteger(res)) {
     console.log(`${num} - в цьому тризначному числі всі цифри однакові і це ${res}`);
 } else if (arrNum[0] === arrNum[1] || arrNum[0] === arrNum[2]) {
     console.log(`${num} - в цьому тризначному числі дві цифри однакові і це ${arrNum[0]}`);
